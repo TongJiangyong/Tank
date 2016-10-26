8ed9d164d425fdbcdd33eb6d180d3a00c13e789f
Tongjiangyong <1246376353@qq.com>
2016-10-26 23:49:25 +0800

    just a test

--- app/src/main/java/yong/tank/MainActivity.java
+++ app/src/main/java/yong/tank/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d(TAG,"Test in company");
+        Log.d(TAG,"just a test");
         setContentView(R.layout.activity_main);
     }
 }
