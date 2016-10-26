6c5fed85d44d1c926cf46b60ae39463c2217f776
TongJiangyong <1246376353@qq.com>
2016-10-26 09:59:36 +0800

    test change some file in company

--- app/src/main/java/yong/tank/MainActivity.java
+++ app/src/main/java/yong/tank/MainActivity.java
@@ -1,13 +1,15 @@
 package yong.tank;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-
+    private  static String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG,"Test in company");
         setContentView(R.layout.activity_main);
     }
 }
